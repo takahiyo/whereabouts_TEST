d403425c1bf8a8b38d9a4c1af7fabd40449f940b
takahiyo <141232864+takahiyo@users.noreply.github.com>
2025-09-08 15:28:43 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// 環境ごとの設定値
+// REMOTE_ENDPOINT: APIのエンドポイント
+// REMOTE_POLL_MS: 状態更新のポーリング間隔(ms)
+// CONFIG_POLL_MS: 設定更新のポーリング間隔(ms)
+// TOKEN_DEFAULT_TTL: トークンのデフォルト有効期限(ms)
+const REMOTE_ENDPOINT = "https://presence-proxy-test.taka-hiyo.workers.dev";
+const REMOTE_POLL_MS = 2000;
+const CONFIG_POLL_MS = 120000;
+const TOKEN_DEFAULT_TTL = 3600000;
