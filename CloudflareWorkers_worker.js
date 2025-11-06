a4a59c43bc7da1e745acce20165ef6aa5720bc52
takahiyo <141232864+takahiyo@users.noreply.github.com>
2025-11-06 09:05:39 +0900

    業務時間リスト修正

--- CloudflareWorkers_worker.js
+++ CloudflareWorkers_worker.js
@@ -4,7 +4,7 @@
 // ・レスポンスは no-store。login/renewの role/office/officeName をそのまま転送
 export default {
   async fetch(req, env, ctx) {
-    const GAS_ENDPOINT = env.GAS_ENDPOINT || "https://script.google.com/macros/s/AKfycbzw7kmlmVLkp9U4WFvmRV7Oe79n8RHAa1A1AcrlMomKjWRzqyOQJzV9QlGaWc7MOt3J/exec";
+    const GAS_ENDPOINT = env.GAS_ENDPOINT || "https://script.google.com/macros/s/AKfycbwx1nNkjmS2Xx47JjucLrYdRQfF08guGOKQaEzJJVRH7KMbLY7afpS4Csm3Ccdq1O2_/exec";
     const origin = req.headers.get('origin') || '';
 
     // CORS 許可元
